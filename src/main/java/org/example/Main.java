6562af0639cd2e1cc558ab67a3457dc81ff30ac1
Vrushang <vrushang@Futurewise.co.in>
2022-11-01 15:58:34 +0530

    gh

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        float b,h,area;
+        b=4;
+        h=13;
+        area=(b*h)/2;
+        System.out.println("Area of triangle isgit :="+area);
+
+    }
+}
